ee747955cfd8984430bce505a2c65d00a7d7e229
SAMUEL GREGORY OGLIARI <samuel.ogliari@acad.univates>
2026-03-31 21:36:22 -0300

    Commit Avaliação01

---
+++ Aula3103Avaliacao/index.js
@@ -0,0 +1,19 @@
+function curiosidade() {
+  window.alert(
+    "Curiosidade muito legal, esses caras são gente boa, a música deles é muito boa e por favor me dê nota roveda, eu sei onde você mora."
+  );
+}
+
+function curiosidade2() {
+  window.alert(
+    "Gêneros vão muito do gosto da pessoa, mas todos sabemos que o Rock é superior."
+  );
+}
+
+let contador = 0;
+const btnCurtir = document.getElementById('curtir');
+function curtir(){
+    contador += 1;
+    window.alert(contador + " vezes ja foi curtida essa informação/imagem")
+}
+
